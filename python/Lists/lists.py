09ddd2081f90771e0bc3fcfc364c12901046bf1f
yesahek <yesahek@gmail.com>
2022-11-20 13:04:15 +0300

    Code_Coach

---
+++ python/Lists/lists.py
@@ -0,0 +1,11 @@
+seats = [
+    ['a','b','c'],
+    ['d','e','f'],
+    ['g','h','i'],
+    ['j','k','l']
+]
+
+input1 = int(input("Enter first Input: "))
+input2 = int(input("Enter second Input: "))
+
+print(seats[input1][input2])
---
+++ python/helloWorld/helloWorld.py
@@ -0,0 +1,7 @@
+commands = ["Lights Off", "Lock the door", "Open the door", "Make Coffee", "Shut down"]
+#your code goes here
+sample = input("Enter Sample: ")
+if sample in commands:
+      print("OK")
+else :
+     print("Not supported")
